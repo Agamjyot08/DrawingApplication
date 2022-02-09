795f6f5ecc3b8b23a88edc96b2543a3b36a36046
Agamjyot08 <singhagamjyot@gmail.com>
2022-02-10 01:32:46 +0530

    New

---
+++ app/src/main/java/com/android/drawingapplication/DrawingView.kt
@@ -0,0 +1,202 @@
+package com.android.drawingapplication
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.MotionEvent
+import android.view.View
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+    private var mPathDraw: Custompath? = null
+    private var mCanvasBitMap: Bitmap? = null
+    private var mDrawPaint: Paint? = null
+    private var mCanvasPaint: Paint? = null
+    private var mBrushSize: Float = 0.toFloat()
+    private var color = Color.BLACK
+    private var Canvas: Canvas? = null
+    private val mpath = ArrayList<Custompath>()
+    private val mrectangle = ArrayList<Rectangle>()
+    private val mCircle = ArrayList<Circle>()
+    private var rectangle: Boolean = false
+    private var currRectangle: Rectangle? = null
+    private var circle: Boolean = false
+    private var currCircle: Circle? = null
+    private var line: Boolean = false
+    private var startX: Float? = 0f
+    private var startY: Float? = 0f
+    private var endX: Float? = 0f
+    private var endY: Float? = 0f
+
+    init {
+        setupdrawing()
+    }
+
+    private fun setupdrawing() {
+        mDrawPaint = Paint()
+        mPathDraw = Custompath(color, mBrushSize)
+        mDrawPaint!!.color = color
+        mDrawPaint!!.style = Paint.Style.STROKE
+        mDrawPaint!!.strokeJoin = Paint.Join.ROUND
+        mDrawPaint!!.strokeCap = Paint.Cap.ROUND
+        mCanvasPaint = Paint(Paint.DITHER_FLAG)
+//        mBrushSize= 20.toFloat()
+
+    }
+
+    // Change of size of canvas map accoering to the device screen size
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        mCanvasBitMap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        Canvas = Canvas(mCanvasBitMap!!)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        for (path in mpath) {
+            mDrawPaint!!.strokeWidth = path.brushthickness
+            mDrawPaint!!.color = path.color
+            canvas.drawPath(path, mDrawPaint!!)
+        }
+
+        canvas.drawBitmap(mCanvasBitMap!!, 0f, 0f, mCanvasPaint)
+        if (!mPathDraw!!.isEmpty) {
+            mDrawPaint!!.strokeWidth = mPathDraw!!.brushthickness
+            mDrawPaint!!.color = mPathDraw!!.color
+            canvas.drawPath(mPathDraw!!, mDrawPaint!!)
+        }
+
+        mrectangle.forEach {
+            mDrawPaint!!.strokeWidth = it.paint.brushthickness
+            mDrawPaint!!.color = it.paint.color
+            canvas.drawRect(it.startX!!, it.startY!!, it.endX!!, it.endY!!, mDrawPaint!!)
+        }
+        currRectangle?.let {
+            mDrawPaint!!.strokeWidth = it.paint.brushthickness
+            mDrawPaint!!.color = it.paint.color
+            canvas.drawRect(it.startX!!, it.startY!!, it.endX!!, it.endY!!, mDrawPaint!!)
+        }
+
+        mCircle.forEach {
+            mDrawPaint!!.strokeWidth = it.paint.brushthickness
+            mDrawPaint!!.color = it.paint.color
+            canvas.drawCircle(it.centerX!!, it.centerY!!, it.radius!!, mDrawPaint!!)
+        }
+
+        currCircle?.let {
+            mDrawPaint!!.strokeWidth = it.paint.brushthickness
+            mDrawPaint!!.color = it.paint.color
+            canvas.drawCircle(it.centerX!!, it.centerY!!, it.radius!!, mDrawPaint!!)
+        }
+
+    }
+
+    // The Event in which when we give input
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val touchx = event?.x
+        val touchy = event?.y
+
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                mPathDraw!!.color = color
+                mPathDraw!!.brushthickness = mBrushSize
+                mPathDraw!!.reset()
+                if (touchx != null)
+                    if (touchy != null) {
+                        startX = touchx
+                        startY = touchy
+                        if (circle) {
+                            currCircle = Circle(touchx, touchy, 0f, Custompath(color, mBrushSize))
+                        }
+                        if (line) {
+                            mPathDraw!!.moveTo(touchx, touchy)
+                        }
+                        if (rectangle) {
+                            currRectangle = Rectangle(startX, startY, startX, startY, Custompath(color, mBrushSize))
+                        }
+                    }
+            }
+            MotionEvent.ACTION_MOVE -> {
+                if (touchx != null) {
+                    if (touchy != null) {
+                        if (line) {
+                            mPathDraw!!.lineTo(touchx, touchy)
+                        }
+                        if (rectangle) {
+                            currRectangle = currRectangle?.copy(
+                                startX = startX,
+                                startY = startY,
+                                endX = touchx,
+                                endY = touchy
+                            )
+                        }
+                        if (circle){
+                            val distance: Float = sqrt((touchx - startX!!).toDouble().pow(2.0) + (touchy - startY!!).toDouble().pow(2.0)).toFloat()
+                            val center = distance/2
+                            currCircle = Circle(startX!! + center, startY!! + center, distance/2, Custompath(color, mBrushSize))
+                        }
+                    }
+                }
+            }
+            MotionEvent.ACTION_UP -> {
+                endX = touchx
+                endY = touchy
+                if (circle){
+                    currCircle = null
+                    val distance = sqrt((endX!! - startX!!).toDouble().pow(2.0) + (endY!! - startY!!).toDouble().pow(2.0)).toFloat()
+                    val center = distance/2
+                    mCircle.add(Circle(startX!! + center, startY!! + center, center, Custompath(color, mBrushSize)))
+                }
+                if (rectangle) {
+                    currRectangle = null
+                    mrectangle.add(Rectangle(startX, startY, endX, endY, Custompath(color, mBrushSize)))
+                }
+                if (line) {
+                    mpath.add(mPathDraw!!)
+                }
+                mPathDraw = Custompath(color, mBrushSize)
+
+            }
+            else -> return false
+        }
+
+        invalidate()
+        return true
+    }
+
+    fun setBrushSize(newSize: Float) {
+        mBrushSize = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            newSize,
+            resources.displayMetrics
+        )
+        mDrawPaint!!.strokeWidth = mBrushSize
+    }
+
+    fun setcolornew(newColor: String) {
+        color = Color.parseColor(newColor)
+        mPathDraw!!.color = color
+    }
+
+    fun setRectangle(set: Boolean) {
+        rectangle = set
+    }
+
+    fun setCircle(set: Boolean) {
+        circle = set
+    }
+
+    fun setLine(set: Boolean) {
+        line = set
+    }
+
+    data class Rectangle(val startX: Float?, val startY: Float?, val endX: Float?, val endY: Float?, val paint: Custompath)
+
+    data class Circle(val centerX: Float?, val centerY: Float?, val radius: Float?, val paint: Custompath)
+
+    inner class Custompath(var color: Int, var brushthickness: Float) : Path() {
+
+    }
+}
---
+++ app/src/main/java/com/android/drawingapplication/MainActivity.kt
@@ -0,0 +1,139 @@
+package com.android.drawingapplication
+
+import android.app.Dialog
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
+import com.android.drawingapplication.R
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.dialog_brush_size.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var mimage_button_current_paint: ImageButton? = null
+    private var mCurrentTool: ImageButton? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mCurrentTool = rel_layout[0] as ImageButton
+//        mCurrentTool!!.setImageDrawable(
+//            ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
+//        )
+
+        ll_paint_colors.visibility = View.INVISIBLE
+        mimage_button_current_paint = ll_paint_colors[3] as ImageButton
+        mimage_button_current_paint!!.setImageDrawable(
+            ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
+        )
+
+        canvasbit.setBrushSize(10.toFloat())
+        ib_brush.setOnClickListener()
+        {
+            show_dialog_brush_size()
+        }
+
+        palette.setOnClickListener() {
+            ll_paint_colors.visibility = View.VISIBLE
+        }
+
+        rect.setOnClickListener() {
+            canvasbit.setLine(false)
+            canvasbit.setRectangle(true)
+            canvasbit.setCircle(false)
+        }
+
+        arrow.setOnClickListener() {
+            canvasbit.setLine(false)
+        }
+
+        pencil.setOnClickListener() {
+            canvasbit.setLine(true)
+            canvasbit.setRectangle(false)
+            canvasbit.setCircle(false)
+        }
+
+        ellipse.setOnClickListener() {
+            canvasbit.setLine(false)
+            canvasbit.setRectangle(false)
+            canvasbit.setCircle(true)
+        }
+    }
+
+    private fun show_dialog_brush_size() {
+        val brushdialog = Dialog(this)
+        brushdialog.setContentView(R.layout.dialog_brush_size)
+        brushdialog.setTitle("BRUSH SIZE")
+        var small = brushdialog.small
+        var medium = brushdialog.medium
+        var large = brushdialog.large
+
+        small.setOnClickListener()
+        {
+            canvasbit.setBrushSize(10.toFloat())
+            brushdialog.dismiss()
+        }
+
+        medium.setOnClickListener()
+        {
+            canvasbit.setBrushSize(20.toFloat())
+            brushdialog.dismiss()
+        }
+
+        large.setOnClickListener()
+        {
+            canvasbit.setBrushSize(30.toFloat())
+            brushdialog.dismiss()
+        }
+        brushdialog.show()
+    }
+
+//    private fun show_dialog_color() {
+//        val colordialog = Dialog(this)
+//        colordialog.setContentView(R.layout.dialog_brush_color)
+//        colordialog.setTitle("BRUSH COLOR")
+//        var red = colordialog.red
+//        var green = colordialog.green
+//        var blue = colordialog.blue
+//        var black = colordialog.black
+//
+//        red.setOnClickListener() {
+//            canvasbit.setcolornew("#FF0000")
+//        }
+//
+//        green.setOnClickListener() {
+//            canvasbit.setcolornew("#FF0000")
+//        }
+//
+//        blue.setOnClickListener() {
+//            canvasbit.setcolornew("#FF0000")
+//        }
+//
+//        black.setOnClickListener() {
+//            canvasbit.setcolornew("#FF0000")
+//        }
+//    }
+
+    fun paintclicked(view: View) {
+        if (view != mimage_button_current_paint) {
+            var imagebuttonpaint = view as ImageButton
+            var newcolor = imagebuttonpaint.tag.toString()
+            canvasbit.setcolornew(newcolor)
+            imagebuttonpaint.setImageDrawable(
+                ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
+            )
+
+            mimage_button_current_paint!!.setImageDrawable(
+                ContextCompat.getDrawable(this, R.drawable.pallet_normal)
+            )
+            mimage_button_current_paint = view
+
+            ll_paint_colors.visibility = View.INVISIBLE
+        }
+
+    }
+}
